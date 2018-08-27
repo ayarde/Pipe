801ae1839f4c91bd5adaed131c2689d3799424d8
Adrián Ayarde <adrian.ayarde@gmail.com>
2018-08-27 12:40:58 -0300

    Create pipes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  name = "Mazinger Z";
+
+  array = [1,2,3,4,5,6,7,8,9,10];
+
+  pi = Math.PI;
+  a = 0.234;
+  salary = 1234.54;
+
+  heroe = {
+    name: "Wolverine",
+    key: "Wolverine",
+    age: 500,
+    address: {
+      street: "Second",
+      house: "20"
+    }
+  };
+
+  promiseValue = new Promise ((resolve, reject)=>{
+      setTimeout( ()=>resolve('Data arrived!'), 3500);
+  });
+
+  date = new Date();
 }
